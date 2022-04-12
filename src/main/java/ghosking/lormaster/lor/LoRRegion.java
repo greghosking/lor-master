b4da16d3a31c484f1d15fb23be31f7909ae1d6c5
Greg Hosking <ghosking@mail.endicott.edu>
2022-04-12 13:58:31 -0400

    Refactored code structure and variables for consistency.

--- src/main/java/ghosking/lormaster/lor/LoRRegion.java
+++ src/main/java/ghosking/lormaster/lor/LoRRegion.java
@@ -40,37 +40,16 @@ public final class LoRRegion {
      * @return An instance of LoRRegion that corresponds to the ID.
      */
     public static LoRRegion fromID(int id) {
-
-        if (id == 0) {
-            return new LoRRegion(1, id, "DE", "Demacia");
-        }
-        if (id == 1) {
-            return new LoRRegion(1, id, "FR", "Freljord");
-        }
-        if (id == 2) {
-            return new LoRRegion(1, id, "IO", "Ionia");
-        }
-        if (id == 3) {
-            return new LoRRegion(1, id, "NX", "Noxus");
-        }
-        if (id == 4) {
-            return new LoRRegion(1, id, "PZ", "Piltover & Zaun");
-        }
-        if (id == 5) {
-            return new LoRRegion(1, id, "SI", "Shadow Isles");
-        }
-        if (id == 6) {
-            return new LoRRegion(2, id, "BW", "Bilgewater");
-        }
-        if (id == 9) {
-            return new LoRRegion(2, id, "MT", "Targon");
-        }
-        if (id == 7) {
-            return new LoRRegion(3, id, "SH", "Shurima");
-        }
-        if (id == 10) {
-            return new LoRRegion(4, id, "BC", "Bandle City");
-        }
+        if (id == 0) return new LoRRegion(1, id, "DE", "Demacia");
+        if (id == 1) return new LoRRegion(1, id, "FR", "Freljord");
+        if (id == 2) return new LoRRegion(1, id, "IO", "Ionia");
+        if (id == 3) return new LoRRegion(1, id, "NX", "Noxus");
+        if (id == 4) return new LoRRegion(1, id, "PZ", "Piltover & Zaun");
+        if (id == 5) return new LoRRegion(1, id, "SI", "Shadow Isles");
+        if (id == 6) return new LoRRegion(2, id, "BW", "Bilgewater");
+        if (id == 9) return new LoRRegion(2, id, "MT", "Targon");
+        if (id == 7) return new LoRRegion(3, id, "SH", "Shurima");
+        if (id == 10) return new LoRRegion(4, id, "BC", "Bandle City");
 
         throw new IllegalArgumentException("Unrecognized region ID: " + id);
     }
@@ -80,37 +59,16 @@ public final class LoRRegion {
      * @return An instance of LoRRegion that corresponds to the code.
      */
     public static LoRRegion fromCode(String code) {
-
-        if (code.compareToIgnoreCase("DE") == 0) {
-            return new LoRRegion(1, 0, "DE", "Demacia");
-        }
-        if (code.compareToIgnoreCase("FR") == 0) {
-            return new LoRRegion(1, 1, "FR", "Freljord");
-        }
-        if (code.compareToIgnoreCase("IO") == 0) {
-            return new LoRRegion(1, 2, "IO", "Ionia");
-        }
-        if (code.compareToIgnoreCase("NX") == 0) {
-            return new LoRRegion(1, 3, "NX", "Noxus");
-        }
-        if (code.compareToIgnoreCase("PZ") == 0) {
-            return new LoRRegion(1, 4, "PZ", "Piltover & Zaun");
-        }
-        if (code.compareToIgnoreCase("SI") == 0) {
-            return new LoRRegion(1, 5, "SI", "Shadow Isles");
-        }
-        if (code.compareToIgnoreCase("BW") == 0) {
-            return new LoRRegion(2, 6, "BW", "Bilgewater");
-        }
-        if (code.compareToIgnoreCase("MT") == 0) {
-            return new LoRRegion(2, 9, "MT", "Targon");
-        }
-        if (code.compareToIgnoreCase("SH") == 0) {
-            return new LoRRegion(3, 7, "SH", "Shurima");
-        }
-        if (code.compareToIgnoreCase("BC") == 0) {
-            return new LoRRegion(4, 10, "BC", "Bandle City");
-        }
+        if (code.compareToIgnoreCase("DE") == 0) return new LoRRegion(1, 0, "DE", "Demacia");
+        if (code.compareToIgnoreCase("FR") == 0) return new LoRRegion(1, 1, "FR", "Freljord");
+        if (code.compareToIgnoreCase("IO") == 0) return new LoRRegion(1, 2, "IO", "Ionia");
+        if (code.compareToIgnoreCase("NX") == 0) return new LoRRegion(1, 3, "NX", "Noxus");
+        if (code.compareToIgnoreCase("PZ") == 0) return new LoRRegion(1, 4, "PZ", "Piltover & Zaun");
+        if (code.compareToIgnoreCase("SI") == 0) return new LoRRegion(1, 5, "SI", "Shadow Isles");
+        if (code.compareToIgnoreCase("BW") == 0) return new LoRRegion(2, 6, "BW", "Bilgewater");
+        if (code.compareToIgnoreCase("MT") == 0) return new LoRRegion(2, 9, "MT", "Targon");
+        if (code.compareToIgnoreCase("SH") == 0) return new LoRRegion(3, 7, "SH", "Shurima");
+        if (code.compareToIgnoreCase("BC") == 0) return new LoRRegion(4, 10, "BC", "Bandle City");
 
         throw new IllegalArgumentException("Unrecognized region code: " + code);
     }
@@ -120,37 +78,16 @@ public final class LoRRegion {
      * @return An instance of LoRRegion that corresponds to the name.
      */
     public static LoRRegion fromName(String name) {
-
-        if (name.compareToIgnoreCase("Demacia") == 0) {
-            return new LoRRegion(1, 0, "DE", "Demacia");
-        }
-        if (name.compareToIgnoreCase("Freljord") == 0) {
-            return new LoRRegion(1, 1, "FR", "Freljord");
-        }
-        if (name.compareToIgnoreCase("Ionia") == 0) {
-            return new LoRRegion(1, 2, "IO", "Ionia");
-        }
-        if (name.compareToIgnoreCase("Noxus") == 0) {
-            return new LoRRegion(1, 3, "NX", "Noxus");
-        }
-        if (name.compareToIgnoreCase("Piltover & Zaun") == 0) {
-            return new LoRRegion(1, 4, "PZ", "Piltover & Zaun");
-        }
-        if (name.compareToIgnoreCase("Shadow Isles") == 0) {
-            return new LoRRegion(1, 5, "SI", "Shadow Isles");
-        }
-        if (name.compareToIgnoreCase("Bilgewater") == 0) {
-            return new LoRRegion(2, 6, "BW", "Bilgewater");
-        }
-        if (name.compareToIgnoreCase("Targon") == 0) {
-            return new LoRRegion(2, 9, "MT", "Targon");
-        }
-        if (name.compareToIgnoreCase("Shurima") == 0) {
-            return new LoRRegion(3, 7, "SH", "Shurima");
-        }
-        if (name.compareToIgnoreCase("Bandle City") == 0) {
-            return new LoRRegion(4, 10, "BC", "Bandle City");
-        }
+        if (name.compareToIgnoreCase("Demacia") == 0) return new LoRRegion(1, 0, "DE", "Demacia");
+        if (name.compareToIgnoreCase("Freljord") == 0) return new LoRRegion(1, 1, "FR", "Freljord");
+        if (name.compareToIgnoreCase("Ionia") == 0) return new LoRRegion(1, 2, "IO", "Ionia");
+        if (name.compareToIgnoreCase("Noxus") == 0) return new LoRRegion(1, 3, "NX", "Noxus");
+        if (name.compareToIgnoreCase("Piltover & Zaun") == 0) return new LoRRegion(1, 4, "PZ", "Piltover & Zaun");
+        if (name.compareToIgnoreCase("Shadow Isles") == 0) return new LoRRegion(1, 5, "SI", "Shadow Isles");
+        if (name.compareToIgnoreCase("Bilgewater") == 0) return new LoRRegion(2, 6, "BW", "Bilgewater");
+        if (name.compareToIgnoreCase("Targon") == 0) return new LoRRegion(2, 9, "MT", "Targon");
+        if (name.compareToIgnoreCase("Shurima") == 0) return new LoRRegion(3, 7, "SH", "Shurima");
+        if (name.compareToIgnoreCase("Bandle City") == 0) return new LoRRegion(4, 10, "BC", "Bandle City");
 
         throw new IllegalArgumentException("Unrecognized region name: " + name);
     }

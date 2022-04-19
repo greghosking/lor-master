3599ac140f0ce6c0bff8e34e0ac045ae55961b31
Greg Hosking <ghosking@mail.endicott.edu>
2022-04-19 10:18:03 -0400

    Refactored and organized FXML and CSS files.

--- src/main/java/ghosking/lormaster/controller/DecksController.java
+++ src/main/java/ghosking/lormaster/controller/DecksController.java
@@ -4,6 +4,8 @@ import ghosking.lormaster.LoRMasterApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,25 +13,23 @@ import java.util.ResourceBundle;
 public class DecksController implements Initializable {
 
     @FXML
-    Button profileNavigationButton;
+    Button profileButton, liveMatchButton, collectionButton, leaderboardButton, metaButton;
     @FXML
-    Button communityNavigationButton;
+    Button newDeckButton, importDeckButton, exportDeckButton, editDeckButton, deleteDeckButton;
+
     @FXML
-    Button collectionNavigationButton;
+    ScrollPane decksScrollPane;
     @FXML
-    Button leaderboardNavigationButton;
-    @FXML
-    Button metaNavigationButton;
+    GridPane decksGridPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         // Link the other navigation buttons to their respective scenes.
-        profileNavigationButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToProfileScene());
-        communityNavigationButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToCommunityScene());
-        collectionNavigationButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToCollectionScene());
-        leaderboardNavigationButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToLeaderboardScene());
-        metaNavigationButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToMetaScene());
+        profileButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToProfileScene());
+        liveMatchButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToLiveMatchScene());
+        collectionButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToCollectionScene());
+        leaderboardButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToLeaderboardScene());
+        metaButton.setOnMouseClicked(mouseEvent -> LoRMasterApplication.switchToMetaScene());
     }
 
 }

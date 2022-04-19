dbd94d09de3fb6cfe5d85778f023b03efcbad81b
Greg Hosking <ghosking@mail.endicott.edu>
2022-04-18 22:22:48 -0400

    Refactored and organized FXML and CSS files.

--- src/main/java/ghosking/lormaster/controller/LoginController.java
+++ src/main/java/ghosking/lormaster/controller/LoginController.java
@@ -15,7 +15,6 @@ import javafx.util.Duration;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
